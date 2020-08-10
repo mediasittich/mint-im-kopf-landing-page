10dbe00a7e264ff1df6bdeb22da242f4ee0e75bc
Regina Galambos <gina.galambos@gmail.com>
2020-08-10 04:01:31 +0200

    create query for reviews component add data

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,64 +1,49 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Testimonials = () => {
+  const data = useStaticQuery(graphql`
+    {
+      strapiReviews {
+        title
+        review {
+          id
+          Title
+          Quote
+          Name
+          Description
+        }
+      }
+    }
+  `)
+  const reviewsList = data.strapiReviews.review.map(rev => {
+    console.log(rev)
+    return (
+      <div className="card" key={rev.id}>
+        <div className="card-body">
+          <h5 className="card-title">{rev.Title}</h5>
+          <p className="card-text">"{rev.Quote}"</p>
+          <p className="card-text">
+            <small>
+              {rev.Name}
+              <br />
+              <span className="text-muted">{rev.Description}</span>
+            </small>
+          </p>
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div className="testimonials-section" id="testimonials-section">
       <div className="container pb-5">
         <div className="row mb-5 justify-content-center">
           <div className="col-lg-7 text-center">
-            <h2 className="section-title">Was meine Schüler sagen</h2>
+            <h2 className="section-title">{data.strapiReviews.title}</h2>
           </div>
         </div>
-        <div className="card-deck">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Super Lehrerin!</h5>
-              <p className="card-text">
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"
-              </p>
-              <p className="card-text">
-                <small>
-                  Manuel
-                  <br />
-                  <span className="text-muted">8. Klasse Gymnasium</span>
-                </small>
-              </p>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Super Lehrerin!</h5>
-              <p className="card-text">
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"
-              </p>
-              <p className="card-text">
-                <small>
-                  Manuel
-                  <br />
-                  <span className="text-muted">8. Klasse Gymnasium</span>
-                </small>
-              </p>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Super Lehrerin!</h5>
-              <p className="card-text">
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"
-              </p>
-              <p className="card-text">
-                <small>
-                  Manuel
-                  <br />
-                  <span className="text-muted">8. Klasse Gymnasium</span>
-                </small>
-              </p>
-            </div>
-          </div>
-        </div>
+        <div className="card-deck">{reviewsList}</div>
       </div>
     </div>
   )
