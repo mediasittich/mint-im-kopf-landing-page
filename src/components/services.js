bffb747ac42f9af0b4352cf31c179ea46cd2fbf2
Regina Galambos <gina.galambos@gmail.com>
2020-08-10 03:30:04 +0200

    create graphql query map data to components add basic styling

--- src/components/services.js
+++ src/components/services.js
@@ -1,145 +1,89 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 import { FiCheck } from "react-icons/fi"
-import IconOne from "../images/undraw_youtube_tutorial.svg"
-import IconTwo from "../images/undraw_teaching.svg"
-import IconThree from "../images/undraw_teacher.svg"
 
 const Services = () => {
-  return (
-    <div className="site-section" id="programs-section">
-      <Container>
-        <Row className="mb-5 justify-content-center">
-          <Col
-            lg={7}
-            className="text-center aos-init"
-            data-aos="fade-up"
-            data-aos-delay
-          >
-            <h2 className="section-title">Our Programs</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
-              repellat aut neque! Doloribus sunt non aut reiciendis, vel
-              recusandae obcaecati hic dicta repudiandae in quas quibusdam
-              ullam, illum sed veniam!
-            </p>
-          </Col>
-        </Row>
-        <Row className="mb-5 align-items-center">
-          <Col
-            lg={7}
-            className="mb-5 aos-init"
-            data-aos="fade-up"
-            data-aos-delay={100}
-          >
-            <IconOne className="img-fluid" />
-          </Col>
-          <Col
-            lg={4}
-            className="ml-auto aos-init"
-            data-aos="fade-up"
-            data-aos-delay={200}
-          >
+  const data = useStaticQuery(graphql`
+    {
+      allStrapiLeistungen {
+        nodes {
+          Titel
+          Intro_Text
+          Gruppe {
+            id
+            List_image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            Item {
+              id
+              content
+            }
+          }
+        }
+      }
+    }
+  `)
+  const [services] = data.allStrapiLeistungen.nodes
+
+  const listGroupList = bullets =>
+    bullets.map(bullet => {
+      return (
+        <div
+          className="d-flex align-items-center custom-icon-wrap mb-3"
+          key={bullet.id}
+        >
+          <span className="custom-icon-inner mr-3">
+            <FiCheck />
+          </span>
+          <div>
+            <strong className="m-0">{bullet.content}</strong>
+          </div>
+        </div>
+      )
+    })
+
+  const listGroup = () =>
+    services.Gruppe.map(category => {
+      return (
+        <div
+          className="row mb-5 align-items-center list-group-block"
+          key={category.id}
+        >
+          <div className="col-lg-7 mb-5 list-group-image">
+            <Img
+              fluid={category.List_image.childImageSharp.fluid}
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-lg-4 ml-auto  list-group-text">
             <h2 className="text-black mb-4">We Are Excellent In Education</h2>
             <p className="mb-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
               maxime nam porro possimus fugiat quo molestiae illo.
             </p>
-            <div className="d-flex align-items-center custom-icon-wrap mb-3">
-              <span className="custom-icon-inner mr-3">
-                <FiCheck />
-              </span>
-              <div>
-                <strong className="m-0">22,931 Yearly Graduates</strong>
-              </div>
-            </div>
-            <div className="d-flex align-items-center custom-icon-wrap">
-              <span className="custom-icon-inner mr-3">
-                <FiCheck />
-              </span>
-              <div>
-                <strong className="m-0">150 Universities Worldwide</strong>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <Row className="mb-5 align-items-center">
-          <Col
-            lg={7}
-            className="mb-5 order-1 order-lg-2 aos-init"
-            data-aos="fade-up"
-            data-aos-delay={100}
-          >
-            <IconTwo className="img-fluid" />
-          </Col>
-          <Col
-            lg={4}
-            className="mr-auto order-2 order-lg-1 aos-init"
-            data-aos="fade-up"
-            data-aos-delay={200}
-          >
-            <h2 className="text-black mb-4">Strive for Excellent</h2>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
-              maxime nam porro possimus fugiat quo molestiae illo.
-            </p>
-            <div className="d-flex align-items-center custom-icon-wrap mb-3">
-              <span className="custom-icon-inner mr-3">
-                <FiCheck />
-              </span>
-              <div>
-                <strong className="m-0">22,931 Yearly Graduates</strong>
-              </div>
-            </div>
-            <div className="d-flex align-items-center custom-icon-wrap">
-              <span className="custom-icon-inner mr-3">
-                <FiCheck />
-              </span>
-              <div>
-                <strong className="m-0">150 Universities Worldwide</strong>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <Row className="mb-5 align-items-center">
-          <Col
-            lg={7}
-            className="col-lg-7 mb-5 aos-init"
-            data-aos="fade-up"
-            data-aos-delay={100}
-          >
-            <IconThree className="img-fluid" />
-          </Col>
-          <Col
-            lg={4}
-            className="ml-auto aos-init"
-            data-aos="fade-up"
-            data-aos-delay={200}
-          >
-            <h2 className="text-black mb-4">Education is life</h2>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
-              maxime nam porro possimus fugiat quo molestiae illo.
-            </p>
-            <div className="d-flex align-items-center custom-icon-wrap mb-3">
-              <span className="custom-icon-inner mr-3">
-                <FiCheck />
-              </span>
-              <div>
-                <strong className="m-0">22,931 Yearly Graduates</strong>
-              </div>
-            </div>
-            <div className="d-flex align-items-center custom-icon-wrap">
-              <span className="custom-icon-inner mr-3">
-                <FiCheck />
-              </span>
-              <div>
-                <strong className="m-0">150 Universities Worldwide</strong>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+            {listGroupList(category.Item)}
+          </div>
+        </div>
+      )
+    })
+
+  console.log(services.Gruppe)
+  return (
+    <div className="site-section" id="services-section">
+      <div className="container">
+        <div className="row mb-5 justify-content-center">
+          <div className="col-lg-7 text-center aos-init">
+            <h2 className="section-title">{services.Titel}</h2>
+            <p>{services.Intro_Text}</p>
+          </div>
+        </div>
+        <div>{listGroup()}</div>
+      </div>
     </div>
   )
 }
