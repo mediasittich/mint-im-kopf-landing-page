355a4eae807c8b7c3deecc69c70396f424112eb1
Regina Galambos <gina.galambos@gmail.com>
2020-08-11 02:28:29 +0200

    add custom styles to about section

--- src/components/about.js
+++ src/components/about.js
@@ -12,28 +12,29 @@ const About = () => {
     }
   `)
   return (
-    <div
-      className="site-section bg-image overlay bg-dark text-white"
-      style={{ backgroundImage: 'url("../images/hero_1.jpg")' }}
+    <section
+      className="about-section site-section bg-image overlay bg-dark text-white"
+      style={{
+        backgroundImage:
+          'url("http://localhost:8000/static/d98c802666261362e19907293f416401/1a900/e343aad9ebac63613abce7ce767be34b.jpg")',
+      }}
     >
       <div className="container">
         <div className="row justify-content-center align-items-center">
-          <div className="col-md-8 text-center testimony">
+          <div className="col-md-8 text-center quote">
+            <h3 className="mb-4">{data.strapiAbout.title}</h3>
             {/* <img
               src="../images/person_4.webp"
               alt="Image"
               className="img-fluid w-25 mb-4 rounded-circle"
             /> */}
-            <h3 className="mb-4">{data.strapiAbout.title}</h3>
             <blockquote>
-              <blockquote>
-                <ReactMarkdown source={data.strapiAbout.description} />
-              </blockquote>
+              <ReactMarkdown source={data.strapiAbout.description} />
             </blockquote>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
