0068bdc8320812897d597a0defef7cc53250a1e8
Regina Galambos <gina.galambos@gmail.com>
2020-08-12 10:03:57 +0200

    change autoplay speed on review cards

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -28,7 +28,7 @@ const Testimonials = () => {
     slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
     responsive: [
       {
         breakpoint: 480,
