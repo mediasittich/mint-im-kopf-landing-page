f965e50a21cd9fdf07b5138c948f282989ffa089
Regina Galambos <gina.galambos@gmail.com>
2024-05-19 13:10:14 +0200

    chore: Update mint color variables and add success style to alert message

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useCallback, useMemo, ChangeEvent } from "react"
 
 const encode = data => {
   return Object.keys(data)
@@ -6,7 +6,32 @@ const encode = data => {
     .join("&")
 }
 
+function useInputValue() {
+  const [value, setValue] = useState("")
+  const handleChange = useCallback(event => {
+    console.log(event.target.value)
+    setValue(event.target.value)
+  }, [])
+  return [value, handleChange]
+}
+
 const ContactForm = () => {
+  const [lastname, setLastname] = useInputValue()
+  const [firstname, setFirstname] = useInputValue()
+  const [phone, setPhone] = useInputValue()
+  const [email, setEmail] = useInputValue()
+  const [textMessage, setTextMessage] = useInputValue()
+
+  const result = useMemo(() => {
+    return JSON.stringify({
+      lastname,
+      firstname,
+      phone,
+      email,
+      textMessage,
+    })
+  }, [lastname, firstname, phone, email, textMessage])
+
   const [formData, setFormData] = useState({
     lastname: "",
     firstname: "",
@@ -15,100 +40,108 @@ const ContactForm = () => {
     textMessage: "",
   })
 
-  const handleChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
+  const handleSubmit = useCallback(
+    event => {
+      event.preventDefault()
+      console.log(result)
 
-  const handleSubmit = e => {
-    console.log(formData)
-    // e.preventDefault()
-    // fetch("/", {
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //   body: JSON.stringify({ "form-name": "contactForm", ...formData }),
-    // })
-    //   .then(() => alert("Success!"))
-    //   .catch(error => alert(error))
-  }
+      // fetch("/", {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      //   body: JSON.stringify({ "form-name": "contactForm", ...formData }),
+      // })
+      //   .then(() => alert("Success!"))
+      //   .catch(error => alert(error))
+    },
+    [result]
+  )
 
   return (
-    <form
-      name="contactForm"
-      method="POST"
-      data-netlify="true"
-      netlify-honeypot="bot-field"
-      onSubmit={handleSubmit}
-    >
-      <p className="d-none">
-        <label>
-          Do not fill this out if you are human: <input name="bot-field" />
-        </label>
-      </p>
-      <input type="hidden" name="form-name" value="contactForm" />
+    <div>
+      <div className="alert-message alert-message-success">
+        <h5 className="card-title">Vielen Dank für Ihre Nachticht!</h5>
+        <p>Ich werde mich schnellstmöglich bei Ihnen melden.</p>
+      </div>
+      <form
+        name="contactForm"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        <p className="d-none">
+          <label>
+            Do not fill this out if you are human: <input name="bot-field" />
+          </label>
+        </p>
+        <input type="hidden" name="form-name" value="contactForm" />
 
-      <div className="form-row">
-        <div className="form-group col-md-6">
-          <label htmlFor="inputLastName">Name</label>
-          <input
-            type="text"
-            name="lastname"
-            id="inputLastName"
-            className="form-control"
-            required
-          />
-        </div>
-        <div className="form-group col-md-6">
-          <label htmlFor="inputFirstName">Vorname</label>
-          <input
-            type="text"
-            name="firstname"
-            id="inputFirstName"
-            className="form-control"
-          />
-        </div>
-      </div>
-      <div className="form-group">
-        <label htmlFor="inputPhone">Telefon</label>
-        <input
-          type="text"
-          name="phone"
-          className="form-control"
-          id="inputPhone"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="inputEmail">Email</label>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <div className="input-group-text">@</div>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="inputLastName">Name</label>
+            <input
+              type="text"
+              name="lastname"
+              id="inputLastName"
+              className="form-control"
+              required
+              value={lastname}
+              onChange={setLastname}
+            />
           </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="inputFirstName">Vorname</label>
+            <input
+              type="text"
+              name="firstname"
+              id="inputFirstName"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputPhone">Telefon</label>
           <input
+            type="text"
+            name="phone"
             className="form-control"
-            id="inputEmail"
-            type="email"
-            name="email"
-            required
+            id="inputPhone"
           />
         </div>
-      </div>
-      <div className="form-group">
-        <label htmlFor="textMessage">Ihre Nachricht</label>
-        <textarea
-          name="textMessage"
-          className="form-control"
-          id="textMessage"
-          rows="5"
-          required
-        ></textarea>
-      </div>
-      <div className="form-group row justify-content-center mt-4">
-        <div>
-          <button className="btn cta" type="submit">
-            Anfrage absenden
-          </button>
+        <div className="form-group">
+          <label htmlFor="inputEmail">Email</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text">@</div>
+            </div>
+            <input
+              className="form-control"
+              id="inputEmail"
+              type="email"
+              name="email"
+              required
+            />
+          </div>
         </div>
-      </div>
-    </form>
+        <div className="form-group">
+          <label htmlFor="textMessage">Ihre Nachricht</label>
+          <textarea
+            name="textMessage"
+            className="form-control"
+            id="textMessage"
+            rows="5"
+            required
+          ></textarea>
+        </div>
+        <div className="form-group row justify-content-center mt-4">
+          <div>
+            <button className="btn cta" type="submit">
+              Anfrage absenden
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
